35ce92d093d0a66fc24cce61f3c545ba925221d6
Chance Linz <clinz.dev@gmail.com>
2023-11-27 13:06:32 -0800

    made the image generation service generic

--- app/services/generation/image.py
+++ app/services/generation/image.py
@@ -1,17 +1,19 @@
 from pydantic import BaseModel
-from typing import List
 
 from .generation import GenerationService
 from .monster import MonsterModel
+from .weapon import WeaponModel
+from .type import GenerationType
+
 
 class ImageGenerationService(GenerationService):
     def __init__(self) -> None:
         super().__init__()
 
-    def generate(self, monster: MonsterModel) -> str:
+    def generate(self, type: GenerationType, data: MonsterModel | WeaponModel) -> str:
         response = self.client.images.generate(
             model="dall-e-3",
-            prompt=f"The following monster for a Dungeons and Dragons campaign: {monster.name} ({monster.description}). The style should match that of images found in Wizards of the Coast Dungeons and Dragons reference material (e.g. The Dungeon Masters Guide & The Players Handbook)",
+            prompt=f"The following {type.value} for a Dungeons and Dragons campaign: {data.name} ({data.description}). The style should match that of images found in Wizards of the Coast Dungeons and Dragons reference material (e.g. The Dungeon Masters Guide & The Players Handbook)",
             size="1024x1024",
             quality="standard",
             n=1
---
+++ app/services/generation/type.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class GenerationType(Enum):
+    MONSTER = 'monster'
+    ITEM = 'item'
+    NAME = 'name'
