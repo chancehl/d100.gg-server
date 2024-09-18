4b1be9cb5c849555d2acbfb89f999480adf1e58b
Chance Linz <clinz.dev@gmail.com>
2024-09-17 20:51:40 -0700

    adds pylint file

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
 from datetime import datetime
+
+from fastapi import FastAPI
 
 from app.api.v1.router import router as api_router
 
 app = FastAPI()
 
+
 @app.get("/ping")
 def ping():
-    return { "now": datetime.now() }
+    return {"now": datetime.now()}
+
 
 app.include_router(api_router, prefix="/api/v1")
